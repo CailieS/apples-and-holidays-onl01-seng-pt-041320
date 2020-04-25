f3693bd847c5708c19fa0098d11ee9692e3837c7
Cailie Skelton <cskelton707@gmail.com>
2020-04-25 18:55:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,20 +66,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-variable = holidays.map do |holiday, supply|
-  holiday if supply.include? ("BBQ")
-holiday_hash.map do |season, holidays|
-holidays.map do |holiday, supply|
+# return an array of holiday names (as symbols) where supply lists
+  # include the string "BBQ"
+variable = holiday_hash.map do |season, holidays|
+holiday.map do |holiday, supply|
   holiday if supply.include? ("BBQ")
   end
 end
+variable.flatten.compact
 end
-flatten.compact
-end
-
-
-
-
-
-
-
